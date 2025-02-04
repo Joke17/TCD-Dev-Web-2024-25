fa8c1fa714765ed5c32ae7b38b65bc9f81c6f012
fefsfs <youthefeels@gmail.com>
2025-02-04 07:54:01 -0300

    Criação do usuario root

---
+++ cargainicial.php
@@ -0,0 +1,22 @@
+<?php
+    include "/rb/rb.php";
+
+    R::setup('mysql:host=127.0.0.1;dbname=tcd2024',
+             'root',
+             '');
+
+    $usuarios = R::findAll('usuarios');
+
+    if($usuarios == null){
+        $usuarios = R::dispense('usuarios');
+        $usuarios->nome = 'root';
+        $usuarios->senha = 'asdf';
+        $usuarios->admin = true;
+
+        $id = R::store($usuarios);
+
+
+    }
+
+    header('Location:index.php');
+?>
