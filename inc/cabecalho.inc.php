22d50fb1e4ac8eccb1f3a86f94aaac4541c2d74a
fefsfs <youthefeels@gmail.com>
2025-02-06 11:48:38 -0300

    atualizacao saudacoes

--- inc/cabecalho.inc.php
+++ inc/cabecalho.inc.php
@@ -8,7 +8,18 @@ if (strpos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
     echo "Você está na página Home.";
 } else {
     if (strpos($_SERVER['REQUEST_URI'], 'sobre.php') == false) {
-        echo "<h3>" . $_SESSION['nome'] . " - Reserva de ambientes - 2024</h3>";
+        $horario = date('g');
+        if($horario > 0 && $horario <12){
+            $saudacao = "Bom dia";
+        }
+        if($horario > 12 && $horario <17){
+            $saudacao = "BOA TARDE";
+        }
+        if($horario > 17 && $horario <= 23){
+            $saudacao = "Boa noite";
+        }
+        echo "<h3> $saudacao " . $_SESSION['nome'] . "!";
+        echo "<br>Reserva de ambientes - 2024</h3><br>";
     }
     echo "<a href=\"Logout.php\">Logout</a> <br>";
     echo "<a href=\"index.php\">Home</a> <br>";
