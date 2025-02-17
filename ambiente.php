2f20838e1f508a2996bc3b0050edb688940341a0
Joke17 <jjsoprofissional@gmail.com>
2025-02-17 09:19:59 -0300

    PRIMEIRO CASO DE CSS COM VIDA PRÓPRIA

--- ambiente.php
+++ ambiente.php
@@ -10,7 +10,7 @@ include_once 'testanome.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ambiente</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <style>
         
     </style>
