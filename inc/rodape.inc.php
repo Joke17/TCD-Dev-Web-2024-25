af25440ce7c419f753f5fbd334a2a2d9bfa093d7
joke17 <jjsoprofissional@gmail.com>
2025-02-25 08:09:18 -0300

    botao para a pagina sobre

--- inc/rodape.inc.php
+++ inc/rodape.inc.php
@@ -1,7 +1,10 @@
 <hr>
-<p>2024.2 IFNMG - Alex Jr. , Ana Elisa, Felipe Gonçalves, João Eduardo & Joaquim Júneo &copy;</p>
 <?php
 if (strpos($_SERVER['REQUEST_URI'], 'sobre.php') !== false || strpos($_SERVER['REQUEST_URI'], 'index.php') !== false) {
     echo "A nossa equipe agradece o acesso!";
+} else {
+    echo "<a href=\"sobre.php\">Sobre Nossa Equipe</a>";
+    
 }
 ?>
+<p>2024.2 IFNMG - Alex Jr. , Ana Elisa, Felipe Gonçalves, João Eduardo & Joaquim Júneo &copy;</p>
